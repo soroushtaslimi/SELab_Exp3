6bb24bc5f48a5297fd9a3d14e17156827f3bbae7
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-11-02 14:46:12 +0330

    add tests for setWidth getWidth

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -20,4 +20,74 @@ public class RectangleTest {
         double area = rectangle.computeArea();
         Assert.assertEquals(17.92, area, EPSILON);
     }
+
+    @Test
+    public void testComputeArea_WHEN_width_6dot1_AND_height_6dot1_EXPECT_37dot21(){
+        Rectangle rectangle = new Rectangle(6.1, 6.1);
+        double area = rectangle.computeArea();
+        Assert.assertEquals(37.21, area, EPSILON);
+    }
+
+    @Test
+    public void testGetWidth_WHEN_width_8_AND_height_5_EXPECT_8(){
+        Rectangle rectangle = new Rectangle(8, 5);
+        double width = rectangle.getWidth();
+        Assert.assertEquals(8, width, EPSILON);
+    }
+
+    @Test
+    public void testGetWidth_WHEN_width_2dot82_AND_height_3dot45_EXPECT_2dot82(){
+        Rectangle rectangle = new Rectangle(2.82, 3.45);
+        double width = rectangle.getWidth();
+        Assert.assertEquals(2.82, width, EPSILON);
+    }
+
+    @Test
+    public void testGetWidth_WHEN_width_4_AND_height_4_EXPECT_4(){
+        Rectangle rectangle = new Rectangle(4, 4);
+        double width = rectangle.getWidth();
+        Assert.assertEquals(4, width, EPSILON);
+    }
+
+    @Test
+    public void testSetWidth_WHEN_8_AND_initially_6_EXPECT_8(){
+        Rectangle rectangle = new Rectangle(6, 11);
+        rectangle.setWidth(8);
+        double width = rectangle.getWidth();
+        Assert.assertEquals(8, width, EPSILON);
+    }
+
+    @Test
+    public void testSetWidth_WHEN_4dot67_AND_initially_4_EXPECT_4dot67(){
+        Rectangle rectangle = new Rectangle(4, 9);
+        rectangle.setWidth(4.67);
+        double width = rectangle.getWidth();
+        Assert.assertEquals(4.67, width, EPSILON);
+    }
+
+    @Test
+    public void testSetWidth_WHEN_4dot67_AND_initially_4dot67_EXPECT_4dot67(){
+        Rectangle rectangle = new Rectangle(4.67, 9);
+        rectangle.setWidth(4.67);
+        double width = rectangle.getWidth();
+        Assert.assertEquals(4.67, width, EPSILON);
+    }
+
+    @Test
+    public void testSetWidth_WHEN_0_AND_initially_4_EXPECT_IllegalArgumentException(){
+        Rectangle rectangle = new Rectangle(4, 9);
+        try {
+            rectangle.setWidth(0);
+            Assert.fail();
+        } catch (IllegalAccessException ignored) {}
+    }
+
+    @Test
+    public void testSetWidth_WHEN_negative_AND_initially_4_EXPECT_IllegalArgumentException(){
+        Rectangle rectangle = new Rectangle(4, 9);
+        try {
+            rectangle.setWidth(-7.4);
+            Assert.fail();
+        } catch (IllegalAccessException ignored) {}
+    }
 }
