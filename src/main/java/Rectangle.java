b53338c9f49d1d07e0f573b083ec818004134f28
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-11-02 15:03:57 +0330

    implement rectangle getter setter

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -11,4 +11,24 @@ public class Rectangle implements HasArea{
     public double computeArea() {
         return width * height;
     }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        if(width <= 0)
+            throw new IllegalArgumentException();
+        this.width = width;
+    }
+
+    public void setHeight(double height) {
+        if(height <= 0)
+            throw new IllegalArgumentException();
+        this.height = height;
+    }
 }
